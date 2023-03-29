f1a4b83ef6f9de5febba5b5676f1b124b7bb3c3f
MikelCruz <mikel.cruz@ikasle.aeg.eus>
2023-03-29 09:57:38 +0200

    [NEW] Creada funcion GameLoop + Creacion de Constantes de estado de pociones

---
+++ client/src/constants.js
@@ -0,0 +1,7 @@
+const State = {
+
+    WAITING:    0,          //Estado de espera para recivir datos 
+    MAKING:     1,          //Estado con los datos ya recibidos y en proceso de trabajo de las pociones
+    DONE:       2,          //Pociones terminadas y listas para ser mostradas
+
+}
--- client/src/game.js
+++ client/src/game.js
@@ -15,3 +15,44 @@ function init()
     initVars();
 
 }
+
+function gameLoop(timeStamp)
+{
+    // console.log("gameloop");
+    //Keep requesting new frames
+
+    //Tiempo real de ciclo de ejecución 
+    const elapsedCycleSeconds = (timeStamp - globals.previousCycleMilliseconds) / 1000;
+
+    //Tiempo anterior de ciclo de ejecución
+    globals.previousCycleMilliseconds = timeStamp;
+
+    //Variable que corrige el tiempo de frame debido a retrasos con respecto al tiempo objetivo (frameTimeObj)
+    globals.deltaTime += elapsedCycleSeconds;
+    // console.log(globals.deltaTime);
+
+    if(globals.deltaTime >= globals.frameTimeObj)
+    {
+        update();
+
+        //Render que Utilizare más adelante para dibujar las imagenes 
+        // render();
+
+        
+        globals.deltaTime -= globals.frameTimeObj;
+    }
+
+    if(globals.gameState === State.PLAYING)
+    {
+        requestAnimationFrame(gameLoop);
+    }
+
+    else
+        cancelAnimationFrame(gameLoop);
+
+}
+
+export 
+{
+    gameLoop
+}
